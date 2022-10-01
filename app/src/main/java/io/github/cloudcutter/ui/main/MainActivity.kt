c68e95e184100ec39bd7f87b547a49f8daf5c741
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2022-10-01 22:06:59 +0200

    Add bottom bar navigation

--- app/src/main/java/io/github/cloudcutter/ui/main/MainActivity.kt
+++ app/src/main/java/io/github/cloudcutter/ui/main/MainActivity.kt
@@ -8,11 +8,16 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
+import androidx.core.view.forEach
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.mikepenz.iconics.IconicsDrawable
+import com.mikepenz.iconics.typeface.IIcon
+import com.mikepenz.iconics.typeface.library.community.material.CommunityMaterial
+import com.mikepenz.iconics.utils.sizeDp
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.cloudcutter.R
 import io.github.cloudcutter.databinding.MainActivityBinding
@@ -38,8 +43,21 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
 		setSupportActionBar(b.toolbar)
 
 		b.toolbar.setupWithNavController(navController)
+		b.bottomNav.setupWithNavController(navController)
 		appBarConfiguration = AppBarConfiguration(navController.graph)
 		setupActionBarWithNavController(navController, appBarConfiguration)
+
+		b.bottomNav.menu.forEach {
+			val icon: IIcon = when (it.itemId) {
+				R.id.menu_devices -> CommunityMaterial.Icon2.cmd_lightbulb_outline
+				R.id.menu_profiles -> CommunityMaterial.Icon.cmd_code_json
+				R.id.menu_dumps -> CommunityMaterial.Icon3.cmd_server
+				else -> CommunityMaterial.Icon2.cmd_help
+			}
+			it.icon = IconicsDrawable(this, icon).apply {
+				sizeDp = 24
+			}
+		}
 	}
 
 	override fun onSupportNavigateUp(): Boolean {
