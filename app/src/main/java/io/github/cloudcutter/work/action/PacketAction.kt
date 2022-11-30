2ddb43f340276478a133377d28767e99faffac0d
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2022-11-30 15:34:37 +0100

    Add better message for SocketTimeout in packet actions

--- app/src/main/java/io/github/cloudcutter/work/action/PacketAction.kt
+++ app/src/main/java/io/github/cloudcutter/work/action/PacketAction.kt
@@ -4,12 +4,16 @@
 
 package io.github.cloudcutter.work.action
 
+import io.github.cloudcutter.R
 import io.github.cloudcutter.util.Text
 import io.github.cloudcutter.work.protocol.base.IPacket
+import java.net.SocketException
 
 class PacketAction(
 	id: String,
 	title: Text,
 	nextId: String?,
 	val packet: IPacket,
-) : Action(id, title, nextId)
+) : Action(id, title, nextId, errorMap = mapOf(
+	SocketException::class.java to Text(R.string.message_error_packet_socket_exception),
+))
