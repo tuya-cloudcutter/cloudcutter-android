c288536cdb6d983f724a5c06306b6870ce365812
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2022-12-13 11:58:32 +0100

    Add activity with crash details

--- app/src/main/java/io/github/cloudcutter/App.kt
+++ app/src/main/java/io/github/cloudcutter/App.kt
@@ -5,6 +5,7 @@
 package io.github.cloudcutter
 
 import android.app.Application
+import cat.ereza.customactivityoncrash.config.CaocConfig
 import com.facebook.stetho.Stetho
 import dagger.hilt.android.HiltAndroidApp
 
@@ -14,5 +15,21 @@ class App : Application() {
 	override fun onCreate() {
 		super.onCreate()
 		Stetho.initializeWithDefaults(this)
+
+		CaocConfig.Builder.create()
+			.backgroundMode(CaocConfig.BACKGROUND_MODE_SILENT)
+			.enabled(true)
+			.showErrorDetails(true)
+			.showRestartButton(true)
+			.logErrorOnRestart(true)
+			.trackActivities(false)
+			.minTimeBetweenCrashesMs(10000)
+			.errorDrawable(R.mipmap.ic_launcher_round)
+			.restartActivity(null)
+			.errorActivity(null)
+			.eventListener(null)
+			.customCrashDataCollector(null)
+			.apply()
+
 	}
 }
